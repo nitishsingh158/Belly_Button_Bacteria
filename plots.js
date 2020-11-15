b94cf7211fca33bd4f0a8be5838aaac050ab4012
nitish <nitish.singh158@gmail.com>
2020-11-15 18:26:36 -0500

    added plot.js

--- plots.js
+++ plots.js
@@ -1,37 +1,51 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+function init() {
+  var selector = d3.select("#selDataset");
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    // Trial Stuff
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+    var sampleIds = data.samples[0].otu_ids;
+    var sampleValues = data.samples[0].sample_values;
+    var idVals = {};
+    sampleIds.map((el,i) => idVals[el]=sampleValues[i]);
+    var keys = Object.entries(idVals).sort((a,b)=>b[1]-a[1]).map(el=>el[0]).slice(0,10);
+    var vals = Object.entries(idVals).sort((a,b)=>b[1]-a[1]).map(el=>el[1]).slice(0,10);
+    console.log(keys,vals);
+    
+    // Trail Stuff End
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
+init();
 
-// data
-var data = [trace1];
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result  = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text("ID: " + result.id);
+    PANEL.append("h6").text("ETHNICITY: " + result.ethnicity);
+    PANEL.append("h6").text("GENDER: " + result.gender);
+    PANEL.append("h6").text("AGE: " + result.age);
+    PANEL.append("h6").text("LOCATION: " + result.location);
+    PANEL.append("h6").text("bbtype: " + result.bbtype);
+    PANEL.append("h6").text("WFREQ: " + result.wfreq);
+
   }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+  );
+}
